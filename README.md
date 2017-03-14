8d18d367c0b6c39e1701f07a81606a0f006197e2
letranloc <letranloc1994@gmail.com>
2017-03-14 19:57:26 +0700

    init project

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# draft-js-katex-plugin
+# DraftJS Katex Plugin
+
+*This is a plugin for the `draft-js-plugins-editor`.*
+
+This plugin insert and render LaTeX using Katex.
+
+## Usage
+
+```js
+import createKatexPlugin from 'draft-js-katex-plugin';
+
+const katexPlugin = createKatexPlugin();
+
+const { InsertButton } = katexPlugin;
+```
+
+## TODO
+
+- [ ] Integrate with [Khan/math-input](https://github.com/Khan/math-input).
---
+++ src/components/InsertKatexButton.js
@@ -0,0 +1,34 @@
+import React, { Component, PropTypes } from 'react';
+import unionClassNames from 'union-class-names';
+import insertTeXBlock from '../modifiers/insertTeXBlock';
+
+export default class InsertKatexButton extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    theme: PropTypes.any,
+  };
+
+  static defaultProps = {
+    theme: {}
+  };
+
+  onClick = () => {
+    const { store } = this.props;
+    const editorState = store.getEditorState();
+    store.setEditorState(insertTeXBlock(editorState));
+  };
+
+  render() {
+    const { theme, className, children } = this.props;
+    const combinedClassName = unionClassNames(theme.insertButton, className);
+
+    return (
+      <button
+        className={combinedClassName}
+        onClick={this.onClick}
+      >
+        {children}
+      </button>
+    );
+  }
+}
